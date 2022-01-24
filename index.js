e0a0fe66830be4ae4e6d3f4a8ba97b9fc69e18d5
Daniella Dutra <danivetdutra@gmail.com>
2022-01-24 19:50:39 -0300

    Criada urls para API

--- index.js
+++ index.js
@@ -8,4 +8,26 @@ const port = 3001;
 app.use(cors());
 app.use(bodyParser.json());
 
+function naoImplementado(req, res) {
+  res.status(501).json({ erro: 'Não implementado' });
+}
+
+// Listar todas as tarefas : GET
+app.get('/gerenciador-tarefas', naoImplementado);
+
+// Listar uma tarefa por id: GET
+app.get('/gerenciador-tarefas/:id', naoImplementado);
+
+// Cadastrar uma tarefa: POST
+app.post('/gerenciador-tarefas', naoImplementado);
+
+// Atualizar uma tarefa: PUT
+app.put('/gerenciador-tarefas/:id', naoImplementado);
+
+// Remover uma tarefa: DELETE
+app.delete('/gerenciador-tarefas/:id', naoImplementado);
+
+// Concluir uma tarefa: PUT ou PATCH
+app.put('/gerenciador-tarefas/:id/concluir', naoImplementado);
+
 app.listen(port, () => console.log(`Servidor inicializado na porta ${port}`));
