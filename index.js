c61a27dae1bfa172fc14af9a45e836743ecd23bd
Daniella Dutra <danivetdutra@gmail.com>
2022-01-24 15:46:38 -0300

    Inserido recursos que serao utilizados pela API

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const cors = require('cors');
 const app = express();
 const port = 3001;
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.listen(port, () => console.log(`Servidor inicializado na porta ${port}`));
